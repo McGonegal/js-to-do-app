53b896a8aa8e1a46113836943863994ba6aa07d0
krisMcGonegal <krismcgonegal@gmail.com>
2023-02-15 15:44:16 -0500

    Planning

---
+++ script.js
@@ -0,0 +1,20 @@
+// Pseudo-code for this app
+
+// 1) save the values/DOM elements we will need to interact with
+// let's query the dom for the form element and save it within a variable
+// let's query the dom for the input element and save it with a variable
+
+// 2) Attach a submit event listener to the form element
+// pass in 2 arguements to the event listener method: the event you are listening for and the callback function which will hold the logic we wish to run once the event occurs on the page
+
+// save the text value from the ouput into a variable
+
+// IF the user submits a to do (check where the value is NOT empty) then:
+    // create an li
+        // add a font awesome icon to it
+        // add the text from the to do the user entered to the li
+
+    // append the li to the ul element
+// ELSE alert the user to please submit a valid to do
+
+// 3) do we track when the task is done?
